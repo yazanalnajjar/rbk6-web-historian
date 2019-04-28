8e3180c1ca8ea94c9cbcb2e9b4efaa83635ec07d
Eshraq <eshraq@geeks.ae>
2019-04-28 18:58:49 +0300

    solution

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var request = require('request');
 var _ = require('underscore');
 
 /*
@@ -22,20 +23,44 @@ exports.initialize = function(pathsObj) {
   });
 };
 
-// The following function names are provided to you to suggest how you might
-// modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  fs.readFile(exports.paths.list, function(err, sites) {
+    sites = sites.toString().split('\n');
+    if (callback) {
+      callback(sites);
+    }
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  exports.readListOfUrls(function(sites) {
+    var found = _.any(sites, function(site, i) {
+      return site.match(url);
+    });
+    callback(found);
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(exports.paths.list, url + '\n', function(err, file) {
+    callback();
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  var sitePath = path.join(exports.paths.archivedSites, url);
+
+  fs.access(sitePath, function(err) {
+    callback(!err);
+  });
 };
 
 exports.downloadUrls = function(urls) {
+  // Iterate over urls and pipe to new files
+  _.each(urls, function (url) {
+    if (!url) { return; }
+    request('http://' + url).pipe(fs.createWriteStream(exports.paths.archivedSites + '/' + url));
+  });
 };
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,65 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
+var url = require('url');
+var helpers = require('./http-helpers');
+
+var actions = {
+  'GET': function (request, response) {
+    // console.log(request
+    var urlPath = url.parse(request.url).pathname;
+
+    // / means index.html
+    if (urlPath === '/') { urlPath = '/index.html'; }
+
+    helpers.serveAssets(response, urlPath, function () {
+      // trim leading slash if present
+      if (urlPath[0] === '/') { urlPath = urlPath.slice(1); }
+
+      archive.isUrlInList(urlPath, function (found) {
+        if (found) {
+          helpers.sendRedirect(response, '/loading.html');
+        } else {
+          helpers.send404(response);
+        }
+      });
+    });
+  },
+  'POST': function (request, response) {
+    // console.log(request);
+    helpers.collectData(request, function (data) {
+      var url = data.split('=')[1].replace('http://', '');
+      console.log(url)
+      // check sites.txt for web site
+      archive.isUrlInList(url, function (found) {
+        if (found) { // found site
+          // check if site is on disk
+          archive.isUrlArchived(url, function (exists) {
+            if (exists) {
+              // redirect to site page (/www.google.com)
+              helpers.sendRedirect(response, '/' + url);
+            } else {
+              // Redirect to loading.html
+              helpers.sendRedirect(response, '/loading.html');
+            }
+          });
+        } else { // not found
+          // add to sites.txt
+          archive.addUrlToList(url, function () {
+            // Redirect to loading.html
+            helpers.sendRedirect(response, '/loading.html');
+          });
+        }
+      });
+    });
+  }
+};
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var handler = actions[req.method];
+  if (handler) {
+    handler(req, res);
+  } else {
+    helpers.send404(response);
+  }
 };
